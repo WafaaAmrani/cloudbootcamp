d198505ced223d822769d69eb0df5b5c659f9aa1
WafaaAmrani <wafaa.amrani@gmail.com>
2023-03-24 10:44:50 +0000

    add simple span processor honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -27,6 +27,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# Show this in the logs within the backend-flask app STOUDT
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
